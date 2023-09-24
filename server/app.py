00255402219837035b49a841aa5d6dc16a12db03
Tong Tang <pichip22@hotmail.com>
2023-09-24 15:14:33 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -18,24 +18,51 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        if username and password:
+            user = User(username = username)
+
+            user.password_hash = password
+
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+            return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            user_dict = user.to_dict()
+            return user_dict, 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
+
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
